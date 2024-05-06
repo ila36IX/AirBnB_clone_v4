9879900273c982707d822c9aa8070e714ba7b0f6
Abdellahsyani <syaniabdos@gmail.com>
2024-05-06 11:46:54 +0100

    done

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+$(document).ready(function () {
+  const checklist = {};
+  const amenityh4 = $('.amenities h4');
+
+  $('.amenities input[type="checkbox"]').on('change', function (){
+    if ($(this).is(':checked')) {
+      checklist[this.dataset.id] = this.dataset.name;
+    } else delete checklist[this.dataset.id];
+    amenityh4.text(Object.values(checklist).join(', ');
+  });
+});
+      
